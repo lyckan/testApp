0882b68ef2c46d1e31450ee74f26fc7cc335104d
Peter Lycksen <peter.lycksen@bwinparty.com>
2015-02-26 15:13:16 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 
-gulp.task('default', function(){
-});
-
-
-
-
 gulp.task('bower', function() {
   return bower()
     .pipe(gulp.dest('lib/'))
+});
+
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname + "/frontend/"));
+  app.listen(4000);
+});
+
+gulp.task('default', ['express'], function() {
+
 });
