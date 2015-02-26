72f86ae244175ac1462d591f72c0def1c2cfa5a7
unknown <lycksen@gmail.com>
2015-02-26 22:53:03 +0100

    livereleoad

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,42 @@ var prefix = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var livereload = require('gulp-livereload');
 var path = require('path');
+var server = require('express');
 
+var cssPath = "frontend/assets/styles/css/";
+var lessPath = "frontend/assets/styles/less/";
 
-var cssPath = "frontend/assets/styles";
-
+var lr;
+function startLivereload() {
+ 
+  lr = require('tiny-lr')();
+  lr.listen(35729);
+}
 
 gulp.task('express', function() {
-  console.log("express");
-
-  var express = require('express');
-
-  var app = express();
-  app.use(express.static(__dirname + "/frontend/"));
+  
+  var app = server();
+  //app.use(require('connect-livereload')());
+  app.use(server.static(__dirname + "/frontend/"));
   app.listen(4000);
+  //startLivereload();
 });
 
 gulp.task('less', function() {
-	console.log("less");
+	gulp.src(lessPath + '**/*.less')
+		.pipe(plumber())
+		.pipe(less())
+		.pipe(gulp.dest(cssPath))
+		.pipe(livereload())
 });
 gulp.task('watch', function() {
-	console.log("watch");
-    gulp.watch('*.less', ['less']);  // Watch all the .less files, then run the less task
+	livereload.listen();
+    gulp.watch(lessPath + '**/*.less', ['less']);  // Watch all the .less files, then run the less task
 });
 
 gulp.task('default', ['watch', 'express'], function() {
 
 });
+
+
+
