b18694c426ea4ecdee98005e14ba7ea7d7da111f
Peter Lycksen <peter.lycksen@bwinparty.com>
2015-02-26 15:49:34 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,33 @@
 var gulp = require('gulp');
-var bower = require('gulp-bower');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var prefix = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
+var path = require('path');
 
-gulp.task('bower', function() {
-  return bower()
-    .pipe(gulp.dest('lib/'))
-});
+
+var cssPath = "frontend/assets/styles";
+
 
 gulp.task('express', function() {
+  console.log("express");
+
   var express = require('express');
+
   var app = express();
   app.use(express.static(__dirname + "/frontend/"));
   app.listen(4000);
 });
 
-gulp.task('default', ['express'], function() {
+gulp.task('less', function() {
+	console.log("less");
+});
+gulp.task('watch', function() {
+	console.log("watch");
+    gulp.watch('*.less', ['less']);  // Watch all the .less files, then run the less task
+});
+
+gulp.task('default', ['watch', 'express'], function() {
 
 });
